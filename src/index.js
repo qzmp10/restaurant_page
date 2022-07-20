0354fa30b7ea486e9dc1cfca29fd3c2dddc2b9ca
qzmp10 <loganrobitaille@outlook.com>
2022-07-20 09:41:44 -0400

    ok. think i finished testing the setup

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import "./style.css"
 
+let content = document.querySelector('#content');
+
 let element = document.createElement('div');
 
 element.textContent = "test";
 
 element.classList.add('red');
+
+content.appendChild(element);
