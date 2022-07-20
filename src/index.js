d97c45f05a738a49c72a55daabf935216be62e9c
qzmp10 <loganrobitaille@outlook.com>
2022-07-20 12:53:29 -0400

    starting the main page base layout

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-import "./style.css"
+import "./style.css";
+import Glizzy from './glizzy.jpg';
 
 let content = document.querySelector('#content');
+content.classList.add('content');
 
-let element = document.createElement('div');
 
-element.textContent = "test";
+const glizzy = new Image();
+glizzy.src = Glizzy;
+content.appendChild(glizzy);
 
-element.classList.add('red');
-
-content.appendChild(element);
